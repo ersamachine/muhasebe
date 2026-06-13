6c5dd115acfbf74c7f1576baa8de5bb697dbacc5
ersamachine <easyboxmachine@gmail.com>
2026-06-13 23:16:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // ====== AYAR ======
 // MUHASEBE Apps Script "Web App" adresini buraya yapıştırın (/exec ile biten).
-window.API_URL = "BURAYA_APPS_SCRIPT_URL_YAPISTIRIN";
+window.API_URL = "https://script.google.com/macros/s/AKfycbyw5pgbykHsORJ-HUZQhRyBRuToqouMViKqgxdle7uHXCkMfNVd6yIHcflDW10Spgu0Pg/exec";
